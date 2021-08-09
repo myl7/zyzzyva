18c73689c7294a9e28b2482ca4af3537832606ad
myl7 <myl@myl.moe>
2021-08-09 23:11:40 +0800

    feat: Update config to fit new situation

--- pkg/conf/config.go
+++ pkg/conf/config.go
@@ -2,41 +2,48 @@ package conf
 
 import (
 	"crypto/rand"
+	"errors"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
-var F = 3
+var F = 1
 var N = 3*F + 1
 var M = 1
 
+//goland:noinspection GoUnusedGlobalVariable
 var KeySize = 2048
 var ClientTimeout = 10 * time.Second
+
+//goland:noinspection GoUnusedGlobalVariable
 var CPInterval = 100
 
 var Extra = []byte("extra")
 
-var UdpMulticastAddr = "224.0.0.1:10000"
-var UdpMulticastInterface = ""
+var UdpMulticastAddr = "224.0.0.1:10001"
+var UdpMulticastInterface = "enp5s0"
+
+var IpPrefix = os.Getenv("IP_PREFIX")
 
 func GetReqAddr(id int) string {
-	p := 0
-	if id < N {
-		p = 20000 + id
-	} else {
-		p = 30000 + id
+	i := strings.LastIndex(IpPrefix, ".")
+	if i == -1 {
+		panic(errors.New("invalid ip prefix"))
 	}
-	return fmt.Sprintf("127.0.0.1:%d", p)
+
+	last, err := strconv.Atoi(IpPrefix[i+1:])
+	if err != nil {
+		panic(err)
+	}
+
+	return fmt.Sprintf("%s.%d:10000", IpPrefix[:i], id+last)
 }
 
-func GetListenAddr(id int) string {
-	p := 0
-	if id < N {
-		p = 20000 + id
-	} else {
-		p = 30000 + id
-	}
-	return fmt.Sprintf(":%d", p)
+func GetListenAddr(_ int) string {
+	return ":10000"
 }
 
 var RandInputSize = 64
